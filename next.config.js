a98cde3e0de3b13b426c9c14b99b86f0f0c1ca05
Bem <minhhieu21193@gmail.com>
2022-11-11 22:09:02 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   images: {
     domains: ["rb.gy"],
   },
-  reactStrictMode: true,
 };
-
-module.exports = nextConfig;
